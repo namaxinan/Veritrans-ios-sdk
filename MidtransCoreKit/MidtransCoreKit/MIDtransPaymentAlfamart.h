7b42e0dead1a9fb5a1dce46b3ca209aa40771ce3
Arie.Prasetiyo <arie>
2019-02-15 13:49:45 +0700

    update new models

---
+++ MidtransCoreKit/MidtransCoreKit/MIDtransPaymentAlfamart.h
@@ -0,0 +1,17 @@
+//
+//  MIDtransPaymentAlfamart.h
+//  MidtransCoreKit
+//
+//  Created by Arie.Prasetiyo on 15/02/19.
+//  Copyright © 2019 Midtrans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MidtransPaymentDetails.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MIDtransPaymentAlfamart : NSObject<MidtransPaymentDetails>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MidtransCoreKit/MidtransCoreKit/MIDtransPaymentAlfamart.m
@@ -0,0 +1,15 @@
+//
+//  MIDtransPaymentAlfamart.m
+//  MidtransCoreKit
+//
+//  Created by Arie.Prasetiyo on 15/02/19.
+//  Copyright © 2019 Midtrans. All rights reserved.
+//
+
+#import "MIDtransPaymentAlfamart.h"
+
+@implementation MIDtransPaymentAlfamart
+- (NSDictionary *)dictionaryValue {
+    return @{@"payment_type":MIDTRANS_PAYMENT_ALFAMART};
+}
+@end
